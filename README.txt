4252abc60cad65a7525142b165854478c29f1839
gretchenthompson <gretchen.thompson3@gmail.com>
2021-03-27 17:34:22 -0400

    add README

---
+++ README.txt
@@ -0,0 +1,18 @@
+# Capstone Project
+
+My project is a website for my massage business.
+The website is a place for clients to find me, get to know me,
+get to know the benefits of massage and contact me.
+It will link them to an online scheduling tool.
+
+## CSS features
+- Flexbox to change from column in mobile view to row wrap in tablet view
+- nth child to rearrange content on desktop view
+- Fixed Header
+- WAVE(web accessibility evaluation tool powered by WebAIM) shows no errors
+
+## JS features
+- Toggle function to show/hide content
+- Recursive function to rotate thru multiple client reviews
+
+
--- main.js
+++ main.js
@@ -1,27 +1,52 @@
+// add click event to toggle button
 document.getElementById("toggle").addEventListener("click", toggleFunction);
 
-function toggleFunction() {
-    const toggle=document.getElementById("definition");
+// on page load hide content div
+document.getElementById("definition").style.display = "none";
 
-    if (toggle.style.display === "none") {
-      toggle.style.display = "block";
+function toggleFunction() {
+  // get hidden content div
+    const toggled = document.getElementById("definition");
+
+    // if div is already hidden, show it
+    if (toggled.style.display === "none") {
+      toggled.style.display = "block";
     } else {
-      toggle.style.display = "none";
+      // else hide it
+      toggled.style.display = "none";
     }
   }
   
-
+// on page load set index to 0
 let reviewIndex = 0;
-showReview();
+
+// get all reviews
+const reviews = document.getElementsByClassName("myReview");
 
 function showReview() {
   
-  const reviews = document.getElementsByClassName("myReview");
+  // hide all reviews
   for (let i = 0; i < reviews.length; i++) {
     reviews[i].style.display = "none";
   }
+
+  // set next review index item
   reviewIndex++;
-  if (reviewIndex > reviews.length) {reviewIndex = 1}
+
+  // if the index item is more than the list of reviews 
+  // is long, reset index back to the first item
+  if (reviewIndex > reviews.length) {
+    reviewIndex = 1;
+  }
+
+  // subtract 1 to get 
+  //to proper index that starts at 0
   reviews[reviewIndex-1].style.display = "block";
+  
+  // after 3 seconds run this function again forever
   setTimeout(showReview, 3000); 
 }
+
+// run function for the first time
+// on page load
+showReview();
